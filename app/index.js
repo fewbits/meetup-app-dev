2feb9298216930fab28e1acf243273b2aeddd3a2
Eric Guimarães de Sousa Silva <eric.gssilva@gmail.com>
2019-04-06 15:57:57 -0300

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+module.exports = app;
+
+console.log("Tiago Ribeiro - tribeiros@indracompany.com")
+app.listen(port, () => console.log(`listening on port ${port}!`))
